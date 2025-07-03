c99f55281a23821335d7af9cf6e79a037cdc51e7
Rolva Mashale <mashalerolva@gmail.com>
2025-07-03 11:43:06 +0200

    added the back end structure

---
+++ gracefund/src/main/java/com/gracefund/gracefund/GracefundApplication.java
@@ -0,0 +1,13 @@
+package com.gracefund.gracefund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GracefundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GracefundApplication.class, args);
+	}
+
+}
---
+++ gracefund/src/test/java/com/gracefund/gracefund/GracefundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gracefund.gracefund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GracefundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
